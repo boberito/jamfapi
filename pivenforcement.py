10a7f0e7bd71ddb9e9d4cabb36a8317ed16fcf2a
Bob Gendler <boberito@mac.com>
2018-08-03 09:32:17 -0400

    Update pivenforcement.py

--- pivenforcement.py
+++ pivenforcement.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python
 
-#Create an extension attribute
-#Create a smart group based off of that extension attribute
-#Scope policies or configuration profiles to that smart group.
-#Now you have an easy way to toggle that setting on/off for a computer without digging through the JSS
+##################################################################
+#: Date Created  : (August 2nd, 2018)
+#: Author        : Bob Gendler
+#: Email         : robert.gendler@nist.gov
+#: Organization  : OISM
+#: Version       : 1.0
+##################################################################
 
 import urllib2
 import base64
 import json
 import sys
 import ssl
-import urllib
 import getpass
 import os
+import subprocess
 
 def login():
     jssuser = raw_input("Enter Your JSS Account:")
@@ -28,12 +31,14 @@ def ***REMOVED***(arg, item):
     pref_path = "/Library/Preferences/com.jamfsoftware.jamf.plist"
     if os.path.exists(pref_path) is True and jamfproserver == "":
         command = "defaults read " + pref_path + " jss_url"
-		jamfproserver = str(os.system(command))
- 
+        jamfproserver = subprocess.check_output(command,stderr=subprocess.STDOUT,shell=True)
+    
     if arg == "-u":
-        return jamfproserver + "JSSResource/users/name/" + item
+        jamfproserver = jamfproserver[:-1] + "JSSResource/users/name/" + item
+        return jamfproserver 
     elif arg == "-c":
-        return jamfproserver + "JSSResource/computers/name/" + item
+        jamfproserver = jamfproserver[:-1] + "JSSResource/computers/name/" + item
+        return jamfproserver
 
 #If you are enabling or disabling function
 def PIVAction(url, action, credentials="MISSING"):
@@ -52,11 +57,11 @@ def PIVAction(url, action, credentials="MISSING"):
         #Extension Attribute ID and Name WILL need to be changed
         #Set EA_ID and EA_name
         #####
-        EA_ID = "934"
+        EA_ID = "497"
         EA_name = ".PIV Enforced"
         ##### 
-        xmldata = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><computer><extension_attributes><extension_attribute><id>" + EA_ID + "</id><name>" + EA_name + "</name><type>String</type><value>" + action + "</value></extension_attribute></extension_attributes></computer>"
-           
+        xmldata = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><computer><extension_attributes><extension_attribute><id>" + EA_ID + "</id><name>" + EA_name + "</name><type>String</type><value>" + action + "</value></extension_attribute></extension_attributes></computer>"     
+        
         try:
             opener = urllib2.build_opener(urllib2.HTTPSHandler)
             request = urllib2.Request(url, data=xmldata)
@@ -74,9 +79,9 @@ def PIVAction(url, action, credentials="MISSING"):
 
         except urllib2.URLError, error:
             #ERROR Checking, returns HTTP Error Codes     
-            print "Something went wrong."
-            print "Error Code: ", error
-        
+            print "Something went wrong.\n", error
+            return error.code
+
     else:
         print action + " is not an appropriate action."
 
@@ -86,26 +91,29 @@ def computerlist(requestURL, credentials="MISSING"):
     if credentials == "MISSING":
         credentials = login()
     try:
-        request = urllib2.Request(url)  
+        request = urllib2.Request(requestURL)   
         request.add_header('Accept', 'application/json')
         request.add_header('Authorization', 'Basic ' + base64.b64encode(credentials['user'] + ':' + credentials['pass']))
         response = urllib2.urlopen(request)
 
         if response.getcode() != "200":
             response_data = json.loads(response.read())
-            print "------------------------------------"
-            print "Full Name: " + response_data['user']['full_name']
-            print "Email: " + response_data['user']['email_address']
-            print "Phone Number: " + response_data['user']['phone_number']
-            print "------------------------------------"
+            print "-----------------------------------------------"
+            print "|\tFull Name: " + response_data['user']['full_name']
+            print "|\tEmail: " + response_data['user']['email_address']
+            print "|\tPhone Number: " + response_data['user']['phone_number']
+            print "-----------------------------------------------"
             computers = response_data['user']['links']['computers']
             for computer in computers:
-                print "Computer: " + computer['name']
+                print "|\tComputer: " + computer['name']
+            print "-----------------------------------------------"
+        return response.getcode()
     except urllib2.URLError, error:
-        #ERROR Checking, returns HTTP Error Codes
-        print "Something went wrong."
-        print "Error Code: ", error
-        
+            #ERROR Checking, returns HTTP Error Codes
+            print "Something went wrong.\n", error
+            return error.code
+    
+    
 #Main Function        
 def main():
     if len(sys.argv) > 1:
@@ -120,20 +128,22 @@ def main():
                 url = ***REMOVED***(options, sys.argv[2])
                 computerlist(url)
             else:
-                print "No username inputted"
+                print "----------------------------"
+                print "*** No username inputted ***"
+                print "----------------------------"
         elif options == "-c":
             if len(sys.argv)  > 3:
                 PIVAction(***REMOVED***(options, sys.argv[2]), sys.argv[3])
             else:
-                print "Missing either computer or action"
+                print "------------------------------"
+                print "*** Missing item or action ***"
+                print "------------------------------"
         else:
             print "Command not found"
     else:
         print "Interactive Mode!"
         print "------------------------------------"
         #INTERACTIVE MODE BEGINS
-        #Loop until not
-        
         apilogin = "MISSING"
         print ""        
         print "   -h \t\t\t\t\t List help"
@@ -142,13 +152,11 @@ def main():
         print "   quit\t\t\t\t\t Type \'quit\' to quit interactive mode"
 
         while True:
-            
-            user_input = raw_input("Please enter an option: ")      
-            #ONLY BREAK when user types quit or q
-            #Nice program realizes you meant to quit when typing q
+
+            user_input = raw_input("Please enter an option: ")
             if user_input.strip().lower() == "quit" or user_input.strip().lower() == "q":
                 break
-                
+            
             the_input = user_input.split(" ", 3)
             the_input += [None] * (3 - len(the_input))
             options, item, action = the_input
@@ -168,10 +176,11 @@ def main():
                         apilogin = login()
 
                     url = ***REMOVED***(options, item)
+                    print url
                     Notsuccessful = str(computerlist(url, apilogin))
                     if Notsuccessful == "401":
-                            apilogin = "MISSING"
-                
+                        apilogin = "MISSING"
+            
             elif options == "-c":
                 if item == None:
                     print "--------------------"
@@ -195,6 +204,6 @@ def main():
                 print "-------------------------"
                 print "*** Command not found ***"
                 print "-------------------------"
-    
+
 if __name__== "__main__":
   main()
