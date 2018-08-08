510501adccba31dfa072885251820c13c9c715c8
Bob Gendler <boberito@mac.com>
2018-08-08 11:39:59 -0400

    Update EADump.py

--- EADump.py
+++ EADump.py
@@ -9,13 +9,17 @@ import os
 import requests
 import requests.packages.urllib3
 
-r=requests.get('https://myjamfpro:8443/JSSResource/computerextensionattributes', auth=('YOUR API USERNAME','YOUR API PASSWORD'), headers={'accept': 'application/json'})
+APIUsername='username'
+APIPassword='password'
+JamfProServer='https://YOURJAMFPROSERVER:8443/'
+
+r=requests.get(JamfProServer + 'JSSResource/computerextensionattributes', auth=(APIUsername,APIPassword), headers={'accept': 'application/json'})
 EAs = r.json()['computer_extension_attributes']
 
 requests.packages.urllib3.disable_warnings()
 
 for record in EAs:
-	z=requests.get('https://myjamfpro:8443/JSSResource/computerextensionattributes/id/%s' % record['id'], auth=('username','password'), headers={'accept': 'application/json'})
+	z=requests.get(JamfProServer + 'JSSResource/computerextensionattributes/id/%s' % record['id'], auth=(APIUsername,APIPassword), headers={'accept': 'application/json'})
 	if z.json()['computer_extension_attribute']['input_type']['type'] == "script":
 		scriptfile = "/Users/Shared/ExtensionAttributes/" + record['name'] + ".sh"
 		contents = z.json()['computer_extension_attribute']['input_type']['script'] 
